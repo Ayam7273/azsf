1577c4b876c5d074f6306eb7528bd52f6f9c143d
Abdulbasit Adio <abdulbasitadio@gmail.com>
2024-10-19 22:30:14 +0100

    3rd commit

--- js/home.js
+++ js/home.js
@@ -1,12 +1,13 @@
-const nav = document.querySelector(".nav"),
-navOpenBtn = document.querySelector(".navOpenBtn"),
-navCloseBtn = document.querySelector(".navCloseBtn");
+// const nav = document.querySelector(".nav"),
+// navOpenBtn = document.querySelector(".navOpenBtn"),
+// navCloseBtn = document.querySelector(".navCloseBtn");
 
-navOpenBtn.addEventListener("click", () => {
-  nav.classList.add("openNav");
-  nav.classList.remove("openSearch");
-  searchIcon.classList.replace("uil-times", "uil-search");
-});
-navCloseBtn.addEventListener("click", () => {
-  nav.classList.remove("openNav");
-});
+// navOpenBtn.addEventListener("click", () => {
+//   nav.classList.add("openNav");
+//   nav.classList.remove("openSearch");
+//   searchIcon.classList.replace("uil-times", "uil-search");
+// });
+// navCloseBtn.addEventListener("click", () => {
+//   nav.classList.remove("openNav");
+// });
+
