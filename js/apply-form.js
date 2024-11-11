86c1f170017218bd3d946f6e197ea7698f52b265
Abdulbasit Adio <abdulbasitadio@gmail.com>
2024-11-11 03:13:21 +0100

    14th commit

---
+++ js/apply-form.js
@@ -0,0 +1,37 @@
+const steps = document.querySelectorAll(".form-step");
+const nextButtons = document.querySelectorAll(".next-button");
+const prevButtons = document.querySelectorAll(".prev-button");
+const progress = document.getElementById("progress");
+const progressSteps = document.querySelectorAll(".progress-step");
+
+let currentStep = 0;
+
+nextButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    steps[currentStep].classList.remove("active");
+    progressSteps[currentStep].classList.remove("active");
+    currentStep++;
+    steps[currentStep].classList.add("active");
+    progressSteps[currentStep].classList.add("active");
+    updateProgress();
+  });
+});
+
+prevButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    steps[currentStep].classList.remove("active");
+    progressSteps[currentStep].classList.remove("active");
+    currentStep--;
+    steps[currentStep].classList.add("active");
+    progressSteps[currentStep].classList.add("active");
+    updateProgress();
+  });
+});
+
+function updateProgress() {
+  const progressPercentage = (currentStep / (steps.length - 1)) * 100;
+  progress.style.width = `${progressPercentage}%`;
+  progressSteps.forEach((step, index) => {
+    step.classList.toggle("completed", index <= currentStep);
+  });
+}
